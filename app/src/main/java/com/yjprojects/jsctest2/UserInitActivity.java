ff0ac6b139424d3b1bd6594c501883216674a323
triangleyj <dpwns99@gmail.com>
2017-08-05 10:31:26 +0900

    Initial commit

---
+++ app/src/main/java/com/yjprojects/jsctest2/UserInitActivity.java
@@ -0,0 +1,10 @@
+package com.yjprojects.jsctest2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jyj on 2017-08-05.
+ */
+
+public class UserInitActivity extends AppCompatActivity {
+}
